78be7386bd150c45610bf3569a5b267569014a81
ClydeWTT <your_email>
2024-09-15 00:20:39 -0400

    add to route

--- EmotiGPT/@latest/src/routes.jsx
+++ EmotiGPT/@latest/src/routes.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import LoginPage from "./pages/Login/LoginPage.jsx";
 import SignUpPage from "./pages/SignUp/SignUpPage.jsx";
@@ -13,8 +18,11 @@ export const RoutesToPages = () => {
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/create-account" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/home" element={<Home />} />
+
+        {/* Redirect "/" path to "/home" */}
+        <Route path="/" element={<Navigate to="/home" replace />} />
       </Routes>
     </Router>
   );
